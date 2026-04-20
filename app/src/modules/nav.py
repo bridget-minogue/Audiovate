ef37ea63555bfae2a3ccd6f9266d5f00bf68ccbd
bridget-minogue <bridgetkminogue@icloud.com>
2026-04-20 18:38:13 -0400

    delete sample persona

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -34,36 +34,6 @@ def artist_stats_nav():
         "pages/02_Artist_Stats.py", label="Artist Stats", icon="📊"
     )
 
-# ---- Role: usaid_worker -----------------------------------------------------
-
-def usaid_worker_home_nav():
-    st.sidebar.page_link(
-        "pages/10_USAID_Worker_Home.py", label="USAID Worker Home", icon="🏠"
-    )
-
-
-def ngo_directory_nav():
-    st.sidebar.page_link("pages/14_NGO_Directory.py", label="NGO Directory", icon="📁")
-
-
-def add_ngo_nav():
-    st.sidebar.page_link("pages/15_Add_NGO.py", label="Add New NGO", icon="➕")
-
-
-def prediction_nav():
-    st.sidebar.page_link(
-        "pages/11_Prediction.py", label="Regression Prediction", icon="📈"
-    )
-
-
-def api_test_nav():
-    st.sidebar.page_link("pages/12_API_Test.py", label="Test the API", icon="🛜")
-
-
-def classification_nav():
-    st.sidebar.page_link(
-        "pages/13_Classification.py", label="Classification Demo", icon="🌺"
-    )
 
 
 # ---- Role: label_head -------------------------------------------------------
@@ -157,8 +127,6 @@ def SideBarLinks(show_home=False):
             artist_library_nav()
             artist_stats_nav()
         
-        if st.session_state["role"] == "data_analyst":
-            pass
 
         if st.session_state["role"] == "label_head":
             label_head_home_nav()
