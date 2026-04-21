f172f7b08d21dd5d00744ac1f80599b347155143
Massimo Mastromattei <mastromattei.ma@northeastern.edu>
2026-04-20 21:36:01 -0400

    fixed import errors and other problems from removing sample files

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -4,7 +4,6 @@ import os
 import logging
 
 from backend.db_connection import init_app as init_db
-from backend.simple.simple_routes import simple_routes
 from backend.audiovate_routes.payoutProfiles.payout_routes import payout_profiles
 from backend.audiovate_routes.assets.asset_routes import assets
 from backend.audiovate_routes.artists.artist_routes import artists
@@ -44,7 +43,6 @@ def create_app():
     # Register the routes from each Blueprint with the app object
     # and give a url prefix to each.
     app.logger.info("create_app(): registering blueprints")
-    app.register_blueprint(simple_routes)
     app.register_blueprint(payout_profiles, url_prefix="/payoutProfiles")
     app.register_blueprint(assets, url_prefix="/assets")
     app.register_blueprint(artists, url_prefix="/artists")
